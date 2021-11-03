b347b7c9ded68b8746aab417d8ae49f8787e3aee
Carlos <cemargato@gmail.com>
2021-11-03 17:11:58 -0300

    estrutura-de-to-do-list

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: 0,
+      task:"",
+      status:"pendente",
+    }
+    this.typeChecker = this.typeChecker.bind(this);
+  }
+
+  typeChecker({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
       <div>
-        
+        <form>
+          <label htmlFor="addTask">
+            <span>Tarefa:</span>
+            &nbsp;
+            <input
+              type="text"
+              name="task"
+              id="addTask"
+              onChange={ this.typeChecker }
+            />
+          </label>
+          <label htmlFor="select">
+            Status:
+            <select id='select' name="status" onChange={ this.typeChecker }>
+              <option value="pendente"  selected>pendente</option>
+              <option value="em andamento">em andamento</option>
+              <option value="pronto">pronto</option>
+            </select>
+          </label>
+          <button id="Salvar" type="submit">Salvar</button>
+        </form>
       </div>
     )
   }
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
 class Todolist extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: 0,
+      task:"",
+      status:"",
+    }
+  }
+
   render() {
+    const { task, status } = this.state;
     return (
-      <div>
-        
-      </div>
+      <>
+        <p>
+          <span>Tarefa:</span>
+          <p>{task}</p>
+        </p>
+        <p>
+          <span>Status:</span>
+          <p>{status}</p>
+        </p>
+      </>
     )
   }
 }
